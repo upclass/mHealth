77400bcb9571ec47fa42036563b512da3c1ec82c
xsm <xsm0824mn003@163.com>
2016-10-31 17:25:13 +0800

    提交两个疾病实体类

--- app/src/main/java/org/caller/mhealth/adapters/DiseaseAdapter.java
+++ app/src/main/java/org/caller/mhealth/adapters/DiseaseAdapter.java
@@ -1,25 +1,75 @@
 package org.caller.mhealth.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.caller.mhealth.R;
+import org.caller.mhealth.entitys.Operation;
+
+import java.util.List;
 
 /**
  * Created by xsm on 16-10-31.
  */
 
 public class DiseaseAdapter extends RecyclerView.Adapter{
+    private Context mContext;
+    private List<Operation> mList;
+
+    public DiseaseAdapter(Context context, List<Operation> list) {
+        mContext = context;
+        mList = list;
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        RecyclerView.ViewHolder ret = null;
+        View itemView = LayoutInflater.from(mContext).inflate(R.layout.item_disease, parent, false);
+        ret = new MyViewHolder(itemView);
+        return ret;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        if (holder instanceof MyViewHolder) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            Operation operation = mList.get(position);
+            myViewHolder.mTxtDepartment.setText(operation.getDepartment());
+            myViewHolder.mTxtPlace.setText(operation.getPlace());
+            myViewHolder.mTxtDescription.setText(operation.getDescription());
+            Glide.with(mContext)
+                    .load("http://tnfs.tngou.net/img" + operation.getImg())
+                    .into(myViewHolder.mImg);
+        }
     }
 
     @Override
     public int getItemCount() {
+        if (mList.size() > 0) {
+            return mList.size();
+        }
         return 0;
+    }
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView mTxtDepartment;
+        private final TextView mTxtPlace;
+        private final ImageView mImg;
+        private final TextView mTxtDescription;
+
+        MyViewHolder(View itemView) {
+            super(itemView);
+            mTxtDepartment = ((TextView) itemView.findViewById(R.id.disease_department));
+            mTxtPlace = ((TextView) itemView.findViewById(R.id.disease_place));
+            mImg = ((ImageView) itemView.findViewById(R.id.disease_img));
+            mTxtDescription = ((TextView) itemView.findViewById(R.id.disease_description));
+        }
     }
 }
--- app/src/main/java/org/caller/mhealth/fragments/DiseaseFragment.java
+++ app/src/main/java/org/caller/mhealth/fragments/DiseaseFragment.java
@@ -2,19 +2,53 @@ package org.caller.mhealth.fragments;
 
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+
 import org.caller.mhealth.R;
+import org.caller.mhealth.adapters.DiseaseAdapter;
 import org.caller.mhealth.base.BaseFragment;
+import org.caller.mhealth.entitys.DiseaseList;
+import org.caller.mhealth.entitys.Operation;
+import org.caller.mhealth.tools.HttpAPI;
+import org.caller.mhealth.tools.HttpTool;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DiseaseFragment extends BaseFragment {
+public class DiseaseFragment extends BaseFragment implements SwipeRefreshLayout.OnRefreshListener, Runnable {
 
+
+    private RecyclerView mRecyclerView;
+    private List<Operation> mList;
+    private String mUrl;
+    private DiseaseAdapter mAdapter;
+    private Handler mHandler = new Handler() {
+        @Override
+        public void dispatchMessage(Message msg) {
+            switch (msg.what) {
+                case 1:
+                    DiseaseList diseaseList = (DiseaseList) msg.obj;
+                    mList = diseaseList.getList();
+                    mAdapter.notifyDataSetChanged();
+                    break;
+            }
+        }
+    };
 
     public DiseaseFragment() {
         // Required empty public constructor
@@ -22,7 +56,7 @@ public class DiseaseFragment extends BaseFragment {
 
     @Override
     public String getTitle() {
-        return "疾病信息";
+        return "常见疾病";
     }
 
 
@@ -30,7 +64,35 @@ public class DiseaseFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_disease, container, false);
+        View ret = inflater.inflate(R.layout.fragment_disease, container, false);
+        mRecyclerView = (RecyclerView) ret.findViewById(R.id.disease_recycler);
+        mList = new ArrayList<>();
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new DiseaseAdapter(getContext(), mList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        SwipeRefreshLayout refreshLayout = (SwipeRefreshLayout) ret.findViewById(R.id.disease_refresh_layout);
+        refreshLayout.setOnRefreshListener(this);
+        mUrl = HttpAPI.getListUrl(1, 20);
+        new Thread(this).start();
+        return ret;
     }
 
+    @Override
+    public void onRefresh() {
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        if (mUrl != null) {
+            byte[] data = HttpTool.getByteResult(mUrl);
+            Gson gson = new Gson();
+            DiseaseList diseaseList = gson.fromJson(new String(data), DiseaseList.class);
+            Message message = mHandler.obtainMessage(1);
+            message.obj = diseaseList;
+            mHandler.sendMessage(message);
+        }
+    }
 }
