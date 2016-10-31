301be116298e10f4da77af23aeb084571e24f5c6
xsm <xsm0824mn003@163.com>
2016-10-31 17:18:06 +0800

    Merge branch 'master' of /home/xsm/project_1/mhealth with conflicts.

---
+++ app/src/main/java/org/caller/mhealth/adapters/DiseaseAdapter.java
@@ -0,0 +1,25 @@
+package org.caller.mhealth.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by xsm on 16-10-31.
+ */
+
+public class DiseaseAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/org/caller/mhealth/tools/HttpAPI.java
@@ -0,0 +1,8 @@
+package org.caller.mhealth.tools;
+
+/**
+ * Created by xsm on 16-10-31.
+ */
+
+public class HttpAPI {
+}
